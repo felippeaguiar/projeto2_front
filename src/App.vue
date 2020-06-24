ee20e482e35f1a6b6c1531cfc977f804108949cc
Felippe Aguiar <felippeneves@hotmail.com>
2020-06-24 15:45:30 -0300

    upload para C2

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">Projeto2 Front</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="/pacientes" class="nav-link">Pacientes</a>
+        </li>
+        <li class="nav-item">
+          <a href="/adicionar" class="nav-link">Adicionar Paciente</a>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+.navbar{
+  box-shadow: 0px 5px 5px 5px black;
+}
+.nav-item a{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 1.0rem;
+  font-weight: 900;
+}
+</style>
